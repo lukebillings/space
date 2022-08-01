8db9cf14a1974a90e8393a6a91b3151cff3e41ab
Luke Billings <lukecbillings@gmail.com>
2022-08-01 22:23:21 +0100

    got destination filter working even with a company having multiple destinations

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,5 +1,9 @@
 class CompaniesController < ApplicationController
   def index
     @companies = Company.all
+    if params.dig(:filters, :destination).present? && params.dig(:filters, :destination) != 0
+        @companies = @companies.select { |company| company.destinations[0] == Destination.find(params[:filters][:destination].to_i) || company.destinations[1] == Destination.find(params[:filters][:destination].to_i) || company.destinations[2] == Destination.find(params[:filters][:destination].to_i)  }
+      end
+      console
   end
 end
