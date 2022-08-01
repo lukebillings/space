10448e411ac552053a68ff91629168db81bae5e7
Luke Billings <lukecbillings@gmail.com>
2022-08-01 22:52:29 +0100

    vehicle filter added + home screen shortcut

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -4,6 +4,8 @@ class CompaniesController < ApplicationController
     if params.dig(:filters, :destination).present? && params.dig(:filters, :destination) != 0
         @companies = @companies.select { |company| company.destinations[0] == Destination.find(params[:filters][:destination].to_i) || company.destinations[1] == Destination.find(params[:filters][:destination].to_i) || company.destinations[2] == Destination.find(params[:filters][:destination].to_i)  }
       end
-      console
+      if params.dig(:filters, :vehicle).present?
+          @companies = @companies.select { |company| company.vehicle ==  params[:filters][:vehicle]}
+        end
   end
 end
