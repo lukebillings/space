c02219c2dadf69a666f5c6ed74a15b714e19f3c2
Luke Billings <lukecbillings@gmail.com>
2022-09-16 17:00:42 +0100

    flights index working with filters

--- app/controllers/company_destinations_controller.rb
+++ app/controllers/company_destinations_controller.rb
@@ -1,6 +1,16 @@
 class CompanyDestinationsController < ApplicationController
   def index
     @company_destinations = CompanyDestination.all
+    if params.dig(:filters, :destination).present? && params.dig(:filters, :destination) != 0
+        @company_destinations = @company_destinations.select { |company_destination| company_destination.destination == Destination.find(params[:filters][:destination].to_i)}
+      end
+      if params.dig(:filters, :vehicle).present?
+          @company_destinations = @company_destinations.select { |company_destination| company_destination.company.vehicle ==  params[:filters][:vehicle]}
+        end
+      if params.dig(:filters, :price).present? && params.dig(:filters, :price) != 0
+         max_price = params[:filters][:price].split("$")[1].split("M")[0].to_f
+         @company_destinations = @company_destinations.select { |company_destination| company_destination.company.starting_price < max_price * 1000000 }
+       end
   end
 end
 
