cdce0517337855b0aacdaa3bb26616d0f30e77f5
Luke Billings <lukecbillings@gmail.com>
2022-10-11 16:29:08 +0100

    changed pandt

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,12 +20,12 @@ companies = Company.create([{ name: 'Space Views' },
     { name: 'SpaceA' }])
 
 
-company_destinations = CompanyDestination.create([{ flight_name: "Voyage 1",company_id: 1, destination_id: 1, price: 100_000, vehicle: 'balloon', distance: 50, next_bookable_flight: 2022 },
+company_destinations = CompanyDestination.create([{ flight_name: "Voyage 1",company_id: 1, destination_id: 1, price: 100_000, vehicle: 'Balloon', distance: 50, next_bookable_flight: 2022 },
 { flight_name: "Earth View", company_id: 2, destination_id: 1, price: 120_000, vehicle: 'Plane', distance: 50, next_bookable_flight: 2024 },
 { flight_name: "New Perspective", company_id: 3, destination_id: 2, price: 300_000, vehicle: 'Rocket', distance: 85, next_bookable_flight: 2025 },
 { flight_name: "Space Journey", company_id: 4, destination_id: 2, price: 400_000, vehicle: 'Rocket', distance: 85, next_bookable_flight: 2026 },
 { flight_name: "Beyond Earth", company_id: 5, destination_id: 3, price: 700_000, vehicle: 'Rocket', distance: 700, next_bookable_flight: 2022 },
-{ flight_name: "Orbital Cruise", company_id: 6, destination_id: 3, price: 1_000_000, vehicle: 'balloon', distance: 700, next_bookable_flight: 2023 },
-{ flight_name: "ISS Voyage", company_id: 7, destination_id: 4, price: 2_000_000, vehicle: 'balloon', distance: 500, next_bookable_flight: 2025  },
+{ flight_name: "Orbital Cruise", company_id: 6, destination_id: 3, price: 1_000_000, vehicle: 'Balloon', distance: 700, next_bookable_flight: 2023 },
+{ flight_name: "ISS Voyage", company_id: 7, destination_id: 4, price: 2_000_000, vehicle: 'Balloon', distance: 500, next_bookable_flight: 2025  },
 { flight_name: "Mission Moon", company_id: 8, destination_id: 5, price: 10_000_000, vehicle: 'Rocket', distance: 500_000, next_bookable_flight: 2026 },
 { flight_name: "Mars Explorer", company_id: 8, destination_id: 6, price: 100_000_000, vehicle: 'Rocket', distance: 400_000_000, next_bookable_flight: 2024 }])
