685c23fd6e88989f5b4ec9bafb29c58f027eaaee
Luke Billings <lukecbillings@gmail.com>
2024-01-17 15:42:08 +0000

    env no equals

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   def contentful
   @client ||= Contentful::Client.new(
-    access_token = ENV['CONTENTFUL_ACCESS_TOKEN'],
-    space_id = ENV['CONTENTFUL_SPACE'],
+    access_token: ENV['CONTENTFUL_ACCESS_TOKEN'],
+    space: ENV['CONTENTFUL_SPACE'],
     dynamic_entries: :auto,
     raise_errors: true
   )
