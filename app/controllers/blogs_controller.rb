bb9b44b643431fae2c0d845b513ffa06eaeb427b
Luke Billings <lukecbillings@gmail.com>
2024-01-19 15:18:01 +0000

    added new image meta tag

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -14,14 +14,14 @@ class BlogsController < ApplicationController
     @blog = contentful.entries(content_type: 'b', include: 10, 'fields.slug[match]' => params[:id]).first
     @renderer = RichTextRenderer::Renderer.new
     set_meta_tags title: @blog.title,
-                   description: @blog.meta_description,
-                   keywords: @blog.keywords,
-                   og: {
-                     title: @blog.title,
-                     description: @blog.meta_description,
-                     type: 'article',
-                     url: blog_url(@blog),
-                     image: @blog.cover.url
-                   }
+                  description: @blog.meta_description,
+                  keywords: @blog.keywords,
+                  og: {
+                    title: @blog.title,
+                    description: @blog.meta_description,
+                    type: 'article',
+                    url: blog_url(@blog),
+                    image: @blog.cover.try(:url)
+                  }
   end
 end
