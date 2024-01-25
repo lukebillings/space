a23e4c9a76ab03a391c21cee1f607396ac4a10a2
Luke Billings <lukecbillings@gmail.com>
2024-01-25 13:42:01 +0000

    truncated blog text not blog hash

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -13,14 +13,36 @@ class BlogsController < ApplicationController
   def show
     @blog = contentful.entries(content_type: 'b', include: 10, 'fields.slug[match]' => params[:id]).first
     @renderer = RichTextRenderer::Renderer.new
+    body_text = extract_text_from_contentful_body(@blog.body)
     set_meta_tags title: @blog.title,
-                    description: @blog.body.truncate(160),
+                    description: truncate_text(body_text, 160),
                     keywords: 'space',
                     og: {
                       title: @blog.title,
                       url: blog_url(@blog),
-                      description: @blog.body.truncate(160),
+                      description: truncate_text(body_text, 160),
                       image: @blog.cover.try(:url)
                     }
   end
+
+
+
+  private
+
+  def extract_text_from_contentful_body(body_hash)
+    # Extract the text content from the Contentful body hash
+    # This depends on the structure of your Contentful data
+    # You might need to adapt this based on your actual Contentful model
+    # For example, if the text is nested within 'content', you might use:
+    # body_hash.dig('content', '0', 'content', '0', 'value')
+    body_hash['content'][0]['content'][0]['value']
+  end
+
+  def truncate_text(text, length)
+    # Truncate the text to the specified length
+    # You might want to handle cases where the text is shorter than the specified length
+    text.truncate(length)
+  end
+
+
 end
