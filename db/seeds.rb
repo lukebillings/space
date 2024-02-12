01a250b3ce79e9702bd18b4d020f58ca4bd8d431
Luke Billings <lukecbillings@gmail.com>
2024-02-12 16:13:40 +0000

    seeds added destroy method

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
    # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+
+Destination.destroy_all
+Company.destroy_all
+CompanyDestination.destroy_all
+
+
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
